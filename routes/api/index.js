2c744f28a24957caad47a275ee1ed56353636207
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-11-28 18:28:28 -0700

    user route get, post, put, delete methods added

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const friendsRoutes = require('./friends');
 const usersRoutes = require('./users');
-const reactionsRoutes = require('./reactions');
 const thoughtsRoutes = require('./thoughts');
 
-router.use('/friends', friendsRoutes);
+
 router.use('/users', usersRoutes);
-router.use('/reactions', reactionsRoutes);
 router.use('/thoughts', thoughtsRoutes)
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,72 @@
 const router = require('express').Router();
+const { User } = require('../../models')
 
+router.get('/', (req, res) => {
+    User.find({}, (err, result) => {
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            console.log('Uh Oh, something went wrong');
+            res.status(500).json({ message: 'something went wrong' })
+        }
+    })
+})
 
+router.get('/:id', (req, res) => {
+    User.findOne({ _id: req.params.id }, (err, result) => {
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            console.log('Uh Oh, something went wrong');
+            res.status(500).json({ message: 'something went wrong' })
+        }
+    })
+})
 
+router.post('/', (req, res) => {
+    const newUser = new User({ username: req.body.username, email: req.body.email })
+    newUser.save();
+    if (newUser) {
+        res.status(200).json(newUser)
+    } else {
+        console.log('Uh Oh, something went wrong');
+        res.status(500).json({ message: 'something went wrong' });
+    }
+})
+
+router.put('/:id', (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            username: req.body.username,
+            email: req.body.email
+        },
+        { new: true },
+        (err, result) => {
+            if (result) {
+                res.status(200).json(result)
+                console.log(`Updated: ${result}`);
+            } else {
+                console.log('Uh Oh, something went wrong');
+                res.status(500).json({ message: 'something went wrong' });
+            }
+        }
+    )
+})
+
+router.delete('/:id', (req, res) => {
+    User.findOneAndDelete({ _id: req.params.id }, (err, result) => {
+        if (result) {
+            res.status(200).json(result);
+            console.log(`Deleted: ${result}`);
+        } else {
+            console.log('Uh Oh, something went wrong');
+            res.status(500).json({ message: 'something went wrong' });
+        }
+    })
+})
+
+// begin friend change routes
+
+router.post('/:id/friends/:friendId')
 module.exports = router;
